d9acb3da995207325dba2812ad86f6a1a67f5d6b
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-08-10 23:31:48 -0600

    Feature Finalizado

--- AlamedasAPI.Infraestructure.Alamedas/DTO/CondominoDTO.cs
+++ AlamedasAPI.Infraestructure.Alamedas/DTO/CondominoDTO.cs
@@ -7,5 +7,8 @@ namespace AlamedasAPI.Infraestructure.Alamedas.DTO
         public int id { get; set; }
         public string nombreCompleto { get; set; }
         public string nombreInquilino { get; set; }
+        public string correo { get; set; }
+        public string Telefono { get; set; }
+        public bool activo { get; set; }
     }
 }
