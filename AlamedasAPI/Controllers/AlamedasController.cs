3080bf3b8aef5994a634915ea2e3d359835a6ac6
Engell Lopez <engelllopez59@gmail.com>
2022-09-02 14:21:18 -0600

    correcion_alias

--- AlamedasAPI/Controllers/AlamedasController.cs
+++ AlamedasAPI/Controllers/AlamedasController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace AlamedasAPI.Controllers;
 
 [ApiController, Authorize]
-[Route("[controller]")]
+[Route("api/[controller]")]
 
 public class AlamedasController : ControllerBase
 {
